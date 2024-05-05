d7bc43c801290abcc175f6245820d93b2ffd7d63
R193571F <167032189+R193571F@users.noreply.github.com>
2024-05-05 11:51:02 +0200

    Delete qgis2web_2024_04_14-18_33_15_556605 directory

--- qgis2web_2024_04_14-18_33_15_556605/script.js
+++
@@ -1,9 +0,0 @@
-// Assume 'styles' contains your predefined marker styles
-var legend = document.getElementById('legend');
-for (var style in styles) {
-    var name = style.name;
-    var icon = style.icon;
-    var div = document.createElement('div');
-    div.innerHTML = '<img src="' + icon + '"> ' + name;
-    legend.appendChild(div);
-}
